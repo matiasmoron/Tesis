bf508680653de4cf8bf5c59c10820ba8855d8829
toniolo77 <franco.toniolo91@gmail.com>
2017-05-20 13:49:37 -0300

    Route

--- react-penna/src/main.jsx
+++ react-penna/src/main.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Fetch from 'react-fetch';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Bs from 'react-bootstrap/lib';
 import container from './componentes/table';
+import counter from './componentes/Counter';
 
 require ("./styles/prueba.scss");
 
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
-    React.createElement(container),
+     <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/counter">Counter</Link></li>
+          </ul>
+          <hr />
+
+          <Route exact path="/" component={container} />
+          <Route path="/counter" component={counter} />
+      </div>
+    </Router>,
     document.getElementById('container')
   );
 });
