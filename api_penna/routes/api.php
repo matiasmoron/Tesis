1b673e345df2d35582c3514d6cab64992f14af22
toniolo77 <franco.toniolo91@gmail.com>
2017-05-29 17:34:37 -0300

    put

--- api_penna/routes/api.php
+++ api_penna/routes/api.php
@@ -25,11 +25,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 //Servicios
 Route::middleware('cors')->get('/servicios', 'Servicio_Controller@get_servicios');
 Route::middleware('cors')->post('/servicios', 'Servicio_Controller@add_servicio');
-Route::middleware('cors')->update('/servicios', 'Servicio_Controller@update_servicio');
+Route::middleware('cors')->put('/servicios', 'Servicio_Controller@update_servicio');
 Route::middleware('cors')->delete('/servicios', 'Servicio_Controller@remove_servicio');
 
 //Puestos
 Route::middleware('cors')->get('/puestos', 'Puesto_Controller@get_puestos');
 Route::middleware('cors')->post('/puestos', 'Puesto_Controller@add_puesto');
-Route::middleware('cors')->update('/puestos', 'Puesto_Controller@update_puesto');
+Route::middleware('cors')->put('/puestos', 'Puesto_Controller@update_puesto');
 Route::middleware('cors')->delete('/puestos', 'Puesto_Controller@remove_puesto');
