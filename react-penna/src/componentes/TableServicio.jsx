aabb18084b1f081991c11bf1f4841866f3cff99b
toniolo77 <franco.toniolo91@gmail.com>
2017-05-29 18:42:50 -0300

    tablas

--- react-penna/src/componentes/TableServicio.jsx
+++ react-penna/src/componentes/TableServicio.jsx
@@ -9,9 +9,33 @@ class TableServicio extends React.Component {
        super();
      }
 
+	onAfterSaveCell(row, cellName, cellValue) {
+		  	console.log(row);
+	}
+
+	onAfterDeleteRow(rowKeys){
+
+	}
+
+
+
    render() {
+	   const cellEditProp = {
+	   	  mode: 'click',
+	   	  blurToSave: true,
+	   	  afterSaveCell: this.onAfterSaveCell  // a hook for after saving cell
+	   	};
+
+		const selectRowProp={
+			mode: 'checkbox'
+		};
+
+		const options= {
+			afterDeleteRow: this.onAfterDeleteRow,
+		};
+
 		 return (
-			 <BootstrapTable height='auto' data={this.props.datos_elemento}  hover>
+			 <BootstrapTable height='auto' search={true} data={this.props.datos_elemento} deleteRow={true} selectRow={selectRowProp}  cellEdit={ cellEditProp } options={options} hover>
 				 <TableHeaderColumn isKey dataField='id_servicio'>ID</TableHeaderColumn>
 				 <TableHeaderColumn dataField='nombre'>Nombre</TableHeaderColumn>
 				 <TableHeaderColumn dataField='edit'>Editar </TableHeaderColumn>
