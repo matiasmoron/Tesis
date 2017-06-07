799c2bcd377dcbd9ced80fe0af5ec23b37374c50
toniolo77 <franco.toniolo91@gmail.com>
2017-06-07 17:01:41 -0300

    personal_tecnico"

--- api_penna/app/Http/Controllers/Tecnico_Controller.php
+++ api_penna/app/Http/Controllers/Tecnico_Controller.php
@@ -8,23 +8,18 @@ use App\Tecnico;
 
 class Tecnico_Controller extends Controller
 {
-    public function get_tecnicos(Request $request){
+    public function get_tecnicos(){
         //echo Tecnico::all();
         $tecnico = DB::table('personal')
                     ->leftJoin('tecnico','tecnico.legajo','=','personal.legajo')
                     ->leftJoin('entidad','tecnico.id_entidad','=','entidad.id_entidad')
-                    ->select('tecnico.legajo', 'personal.nombre','dni','entidad.nombre as entidad','entidad.id_entidad');
-
-        if (isset($request->dni)){
-            $tecnico= $tecnico->where('personal.dni', $request->dni);
-        }
-
-        $tecnico= $tecnico->get();
+                    ->select('tecnico.legajo', 'personal.nombre','dni','entidad.nombre as entidad','entidad.id_entidad')
+                    ->get();
+        
         return $tecnico;
     }
 
-    /*public function get_tecnico_personal(Request $request){
-        echo $request;
+    public function get_tecnico_personal(Request $request){
         $tecnico = DB::table('personal')
                     ->join('tecnico','tecnico.legajo','=','personal.legajo')
                     ->join('entidad','tecnico.id_entidad','=','entidad.id_entidad')
@@ -33,7 +28,7 @@ class Tecnico_Controller extends Controller
                     ->get();
 
         return $tecnico;
-    }*/
+    }
 
     public function add_tecnico(Request $request){
     	$tecnico = new Tecnico;
