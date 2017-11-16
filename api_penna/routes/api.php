304f4e718ec4a5cc5d1324cc684f7070d548b35e
toniolo77 <franco.toniolo91@gmail.com>
2017-11-16 16:53:14 -0300

    ruta existe equipo

--- api_penna/routes/api.php
+++ api_penna/routes/api.php
@@ -58,6 +58,7 @@ Route::middleware('jwt.auth')->delete('/entidades', 'Entidad_Controller@remove_e
 
 //Equipo
 Route::middleware('jwt.auth')->post('/equipos/padre', 'Equipo_Controller@get_padres');
+Route::middleware('jwt.auth')->post('/equipos/cod_patrimonial', 'Equipo_Controller@existe_cod_patrimonial');
 Route::middleware('jwt.auth')->get('/equipos/{id_equipo?}', 'Equipo_Controller@get_equipos');
 Route::middleware('jwt.auth')->post('/equipos', 'Equipo_Controller@add_equipo');
 Route::middleware('jwt.auth')->put('/equipos', 'Equipo_Controller@update_equipo');
